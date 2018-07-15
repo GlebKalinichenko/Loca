fac63217d274e15f5f45dcf2eea6c2dd71923c9d
GlebKalinichenko <gleb.kalinichenko@umbrella-web.com>
2018-07-15 19:13:28 +0300

    Implemented sample for kotlin and java

---
+++ locasample/app/src/main/java/codelab/android/library/locasample/JavaMainActivity.java
@@ -0,0 +1,56 @@
+package codelab.android.library.locasample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import org.jetbrains.annotations.NotNull;
+
+import codelab.android.library.loca.delegate.LocationDelegate;
+import codelab.android.library.loca.entity.GeoEntity;
+import codelab.android.library.loca.entity.LocationConfiguration;
+import codelab.android.library.loca.subscriber.LocationSubscriber;
+import codelab.android.locationbuilder.library.LocationSubscriber;
+import codelab.android.locationbuilder.library.delegate.LocationDelegate;
+import codelab.android.locationbuilder.library.entity.GeoEntity;
+import codelab.android.locationbuilder.library.entity.LocationConfiguration;
+import codelab.android.locationbuilder.library.entity.LocationSubscriberInstance;
+
+public class JavaMainActivity extends AppCompatActivity  {
+
+    private LocationConfiguration configuration = new LocationConfiguration.LocationConfigurationBuilder().build();
+    private LocationSubscriber subscriber;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        subscriber = LocationSubscriberInstance.getLocationSubscriber().instance(getApplicationContext(), configuration);
+        subscriber.registerLocation(new LocationDelegate() {
+
+            @Override
+            public void permissionFailed() {
+                Log.d("", "");
+            }
+
+            @Override
+            public void settingsFailed(@NotNull Exception exception) {
+                Log.d("", "");
+            }
+
+            @Override
+            public void successLocation(@NotNull GeoEntity point) {
+                Log.d("", "");
+            }
+
+            @Override
+            public void failedLocation(@NotNull Exception exception) { Log.d("", exception.getMessage()); }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        subscriber.unregisterLocation();
+        super.onStop();
+    }
+}
